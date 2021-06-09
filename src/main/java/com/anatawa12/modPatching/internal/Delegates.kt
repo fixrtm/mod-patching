5b1e292f434252ac37d09bf7ef943dc596fd7f4e
anatawa12 <anatawa12@icloud.com>
2021-06-09 18:54:58 +0900

    improve delegates

--- src/main/java/com/anatawa12/modPatching/internal/Delegates.kt
+++ src/main/java/com/anatawa12/modPatching/internal/Delegates.kt
@@ -3,80 +3,56 @@ package com.anatawa12.modPatching.internal
 import kotlin.reflect.KProperty
 
 internal object Delegates {
-    fun <T> lazy() = FreezableLazyValueFactory<T>(
-        checkFreezeFactory = noFreezeFactory,
-        defaultFactory = notInitializedFactory,
-        init = null,
-    )
+    fun <T> lazy() = NonFreezableDelegatedValue<T>(DefaultHolder(notInitialized))
 
-    fun <T> lazyFreezable() = OnContainerLazyValueFactory<T>(
-        defaultFactory = notInitializedFactory,
-        init = null,
-    )
+    fun <T> lazyFreezable() = FreezableDelegatedValue<T>(DefaultHolder(notInitialized))
 
-    fun <T> freezable(init: T) = OnContainerLazyValueFactory(
-        defaultFactory = notInitializedFactory,
-        init = init.wrapNull(),
-    )
+    fun <T> freezable(init: T) = FreezableDelegatedValue<T>(init.wrapNull())
 
-    fun <T> withDefault(default: () -> T) = FreezableLazyValueFactory(
-        checkFreezeFactory = noFreezeFactory,
-        defaultFactory = { default },
-        init = null,
-    )
+    fun <T> withDefault(default: () -> T) = NonFreezableDelegatedValue<T>(DefaultHolder { default() })
 
-    fun <T> withDefaultFreezable(default: () -> T) = OnContainerLazyValueFactory(
-        defaultFactory = { default },
-        init = null,
-    )
+    fun <T> withDefaultFreezable(default: () -> T) = FreezableDelegatedValue<T>(DefaultHolder { default() })
 
-    private val noFreezeFactory: (String) -> (() -> Unit) = {{}}
-    private val notInitializedFactory: (String) -> (() -> Nothing) = {{ error("$it is not initialized") }}
+    private val notInitialized: (String) -> Nothing = { error("$it is not initialized") }
 }
 
-internal class DelegatedValue<T> (
-    val checkFreeze: () -> Unit,
-    val default: () -> T,
-    init: T?,
-) {
-    private var valueInternal: T? = init
-    var value: T
-        get() = (valueInternal ?: default()).unwrapNull()
-        set(value) {
-            checkFreeze()
-            valueInternal = value.wrapNull()
-        }
+internal class DefaultHolder<T>(val func: (String) -> T)
+
+@Suppress("UNCHECKED_CAST")
+private fun <T> processWrapped(valueInternal: Any?, name: String): T =
+    (valueInternal as? DefaultHolder<T>)?.func?.invoke(name) ?: (valueInternal as T).unwrapNull<T>()
+
+// init: NULL or T or DefaultHolder<T>
+internal class FreezableDelegatedValue<T> (init: Any?) {
+    internal var valueInternal: Any? = init
 
     @Suppress("NOTHING_TO_INLINE")
-    inline operator fun getValue(thisRef: Any?, property: KProperty<*>): T = value
+    inline operator fun getValue(thisRef: FreezableContainer, property: KProperty<*>): T =
+        processWrapped(valueInternal, property.name)
 
     @Suppress("NOTHING_TO_INLINE")
-    inline operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-        this.value = value
+    inline operator fun setValue(thisRef: FreezableContainer, property: KProperty<*>, value: T) {
+        thisRef.checkFreeze(property.name)
+        this.valueInternal = value.wrapNull()
     }
 }
 
-internal class FreezableLazyValueFactory<T>(
-    val checkFreezeFactory: (String) -> (() -> Unit),
-    val defaultFactory: (String) -> (() -> T),
-    val init: T?,
-) {
-    @Suppress("NOTHING_TO_INLINE")
-    inline operator fun provideDelegate(thisRef: Any?, property: KProperty<*>) =
-        DelegatedValue(checkFreezeFactory(property.name), defaultFactory(property.name), init)
-}
+// init: NULL or T or DefaultHolder<T>
+internal class NonFreezableDelegatedValue<T> (init: Any?) {
+    internal var valueInternal: Any? = init
 
-internal class OnContainerLazyValueFactory<T>(
-    val defaultFactory: (String) -> (() -> T),
-    val init: T?,
-) {
     @Suppress("NOTHING_TO_INLINE")
-    inline operator fun provideDelegate(thisRef: FreezableContainer, property: KProperty<*>) =
-        DelegatedValue({ thisRef.checkFreeze(property.name) }, defaultFactory(property.name), init)
+    inline operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+        processWrapped(valueInternal, property.name)
+
+    @Suppress("NOTHING_TO_INLINE")
+    inline operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.valueInternal = value.wrapNull()
+    }
 }
 
 private val NULL = Any()
 @Suppress("UNCHECKED_CAST")
-private fun <T> T.wrapNull(): T = this ?: NULL as T
+private fun <T : Any> T?.wrapNull(): T = this ?: NULL as T
 @Suppress("UNCHECKED_CAST")
 private fun <T> T.unwrapNull(): T =  this.takeUnless { it === NULL } as T
