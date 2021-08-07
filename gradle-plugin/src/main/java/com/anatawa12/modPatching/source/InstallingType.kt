218b609a9f458f98b3fa14a87b4105206248046b
anatawa12 <anatawa12@icloud.com>
2021-08-07 22:40:53 +0900

    add installingType to InstallSourcePatchingUtil

---
+++ gradle-plugin/src/main/java/com/anatawa12/modPatching/source/InstallingType.kt
@@ -0,0 +1,23 @@
+package com.anatawa12.modPatching.source
+
+import com.anatawa12.modPatching.source.internal.OperatingSystem
+
+enum class InstallingType {
+    Symlink,
+    Copying,
+    ;
+
+    companion object {
+        val default by lazy {
+            when (OperatingSystem.current) {
+                // on posix, it works well
+                OperatingSystem.LINUX -> Symlink
+                OperatingSystem.MACOS -> Symlink
+                // on Windows, symlink doesn't work well
+                OperatingSystem.WINDOWS -> Copying
+                // for safety, use copy
+                null -> Copying
+            }
+        }
+    }
+}
