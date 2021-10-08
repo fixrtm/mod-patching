4ca74cab6e92e885c1fcadf6846e5ab940403b06
anatawa12 <anatawa12@icloud.com>
2021-10-09 00:01:56 +0900

    use DisableCachingByDefault for RenameSourceName

--- gradle-plugin/src/main/java/com/anatawa12/modPatching/binary/RenameSourceName.kt
+++ gradle-plugin/src/main/java/com/anatawa12/modPatching/binary/RenameSourceName.kt
@@ -3,14 +3,17 @@ package com.anatawa12.modPatching.binary
 import com.anatawa12.modPatching.internal.SourceRenamer
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
-import org.gradle.kotlin.dsl.property
+import org.gradle.kotlin.dsl.*
+import org.gradle.work.DisableCachingByDefault
 import java.io.File
 
+@DisableCachingByDefault
 open class RenameSourceName : DefaultTask() {
-    @OutputDirectory
+    @Internal
     val classesDir = project.objects.property(File::class)
+
     @Input
     val suffix = project.objects.property(String::class)
 
