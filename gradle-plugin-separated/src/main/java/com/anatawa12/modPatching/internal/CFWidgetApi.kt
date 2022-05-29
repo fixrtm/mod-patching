1b17d6f7f170ac26da35fe6170259081f558f5e5
anatawa12 <anatawa12@icloud.com>
2022-05-29 17:03:20 +0900

    add request information to exception message

--- gradle-plugin-separated/src/main/java/com/anatawa12/modPatching/internal/CFWidgetApi.kt
+++ gradle-plugin-separated/src/main/java/com/anatawa12/modPatching/internal/CFWidgetApi.kt
@@ -9,7 +9,7 @@ internal object CFWidgetApi {
         val req = HttpGet("https://api.cfwidget.com/minecraft/mc-mods/$id")
         client.execute(req).use { res ->
             if (res.statusLine.statusCode !in 200 until 300)
-                throw IOException("unexpected code: $res")
+                throw IOException("unexpected response code: \n\t\trequrest: $req\n\t\t$res")
             return Json.decodeFromString(CFWidgetProject.serializer(),
                 res.entity.content.reader().buffered().use { it.readText() })
         }
